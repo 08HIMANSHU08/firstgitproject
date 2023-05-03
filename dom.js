e45f982c42c6ffc91e7713939340667639b777ec
08HIMANSHU08 <himanshusinghpatel@gmail.com>
2023-05-03 12:08:01 +0530

    indexdom html file, getElementByClassName

--- dom.js
+++ dom.js
@@ -16,14 +16,31 @@
 // GETELEMENTBYID
 
 // console.log(document.getElementById("header-title"));
-let headerTitle=document.getElementById("header-title");
-let header=document.getElementById("main-header");
-console.log(headerTitle);
-headerTitle.textContent="Hello World";
-headerTitle.innerText="goodbye";
-console.log(headerTitle.innerText);
-// headerTitle.innerText="<h3>Hello</h3>";
-header.style.borderBottom="solid 3px #000";
-let title=document.getElementsByClassName("title");
-title[0].style.color="green";
-title[0].style.fontWeight="bold";
+// let headerTitle=document.getElementById("header-title");
+// let header=document.getElementById("main-header");
+// //console.log(headerTitle);
+// //headerTitle.textContent="Hello World";
+// //headerTitle.innerText="goodbye";
+// console.log(headerTitle.textContent);
+// console.log(headerTitle.innerText);
+// // headerTitle.innerText="<h3>Hello</h3>";
+// header.style.borderBottom="solid 3px #000";
+
+//GETELEMENTBYCLASSNAME
+
+var items=document.getElementsByClassName("list-group-item");
+// console.log('items');
+// console.log('items[1]');
+// items[1]=textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroungColor='yellow';
+// for(let i=0;i<items.length;i++)
+// {
+//     items[i].style.backgroundColor="#f4f4";
+// }
+
+items[2].style.backgroundColor='green';
+for(let i=0;i<items.length;i++)
+{
+    items[i].style.fontWeight='bold';
+}
